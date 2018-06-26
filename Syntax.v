e4fc2f2a5528a5fe1ad125e4b6b8217430762e30
Murali Vijayaraghavan <vmurali@sifive.com>
2018-06-26 13:09:33 -0700

    made switch syntax only parsing. fixed inlining

--- Syntax.v
+++ Syntax.v
@@ -522,17 +522,17 @@ Notation "s @% f" := (ReadStruct s ltac:(let typeS := type of s in
                        (at level 38) : kami_expr_scope.
 
 Notation "name ::= value" :=
-  (name, value): kami_switch_init_scope.
+  (name, value) (only parsing): kami_switch_init_scope.
 Delimit Scope kami_switch_init_scope with switch_init.
-
-Notation "'Switch' val 'Of' inK 'Retn' retK 'With' { s1 ; .. ; sN }" :=
-  (unpack retK (CABit Bor (cons ((SignExtend _ (pack (val == (fst s1%switch_init: Expr _ (SyntaxKind inK))))) & pack (snd s1%switch_init))%kami_expr ..
-                                (cons ((SignExtend _ (pack (val == (fst sN%switch_init: Expr _ (SyntaxKind inK))))) & pack (snd sN%switch_init))%kami_expr nil) ..))):
-    kami_expr_scope.
 
 Notation "'Switch' val 'Retn' retK 'With' { s1 ; .. ; sN }" :=
   (unpack retK (CABit Bor (cons ((SignExtend _ (pack (val == (fst s1%switch_init)))) & pack (snd s1%switch_init))%kami_expr ..
                                 (cons ((SignExtend _ (pack (val == (fst sN%switch_init)))) & pack (snd sN%switch_init))%kami_expr nil) ..))):
+    kami_expr_scope.
+
+Notation "'Switch' val 'Of' inK 'Retn' retK 'With' { s1 ; .. ; sN }" :=
+  (unpack retK (CABit Bor (cons ((SignExtend _ (pack (val == (fst s1%switch_init: Expr _ (SyntaxKind inK))))) & pack (snd s1%switch_init))%kami_expr ..
+                                (cons ((SignExtend _ (pack (val == (fst sN%switch_init: Expr _ (SyntaxKind inK))))) & pack (snd sN%switch_init))%kami_expr nil) ..))):
     kami_expr_scope.
 
 Definition testSwitch ty (val: Bit 5 @# ty) (a b: Bool @# ty) : Bool @# ty :=
