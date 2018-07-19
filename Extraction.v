ef7ee81ad13cbaa1092a567eb1fc424117a5722e
Murali Vijayaraghavan <vmurali@sifive.com>
2018-07-19 14:13:16 -0700

    fixed the string stuff in coq

--- Extraction.v
+++ Extraction.v
@@ -1,7 +1,17 @@
-Require Export List String.
+Require Export List String Ascii.
 Require Export Syntax Compile Rtl.
 
-Require Export ExtrHaskellBasic ExtrHaskellNatInt ExtrHaskellString.
+Require Coq.extraction.Extraction.
+
+Require Export ExtrHaskellBasic ExtrHaskellNatInt.
+
+Extract Inductive ascii => "Prelude.Char"
+  [ "(\b0 b1 b2 b3 b4 b5 b6 b7 -> Prelude.toEnum ( (if b0 then 1 else 0) Prelude.+ (if b1 then 2 else 0) Prelude.+ (if b2 then 4 else 0) Prelude.+ (if b3 then 8 else 0) Prelude.+ (if b4 then 16 else 0) Prelude.+ (if b5 then 32 else 0) Prelude.+ (if b6 then 64 else 0) Prelude.+ (if b7 then 128 else 0)))" ]
+  "(\f a -> f (testbit (Prelude.fromEnum a) 0) (testbit (Prelude.fromEnum a) 1) (testbit (Prelude.fromEnum a) 2) (testbit (Prelude.fromEnum a) 3) (testbit (Prelude.fromEnum a) 4) (testbit (Prelude.fromEnum a) 5) (testbit (Prelude.fromEnum a) 6) (testbit (Prelude.fromEnum a) 7))".
+Extract Inlined Constant Ascii.ascii_dec => "(Prelude.==)".
+
+Extract Inductive string => "Prelude.String" [ "([])" "(:)" ].
+Extract Inlined Constant String.string_dec => "(Prelude.==)".
 
 Extraction Language Haskell.
 
@@ -16,6 +26,12 @@ Extract Inlined Constant projT1 => "Prelude.fst".
 Extract Inlined Constant projT2 => "Prelude.snd".
 Extract Inlined Constant map => "Prelude.map".
 Extract Inlined Constant concat => "Prelude.concat".
+
+
+(* Require Import HexNotationWord. *)
+(* Definition test := 8'h"deadbeef". *)
+(* Extraction "Test.hs" test Nat.testbit. *)
+
 
 (* Extraction "Target.hs" RtlModule size. *)
 
