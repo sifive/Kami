9fb646c2841db09e90f1d3fdea29bae3cf883983
Jay Kruer <jay.kruer@sifive.com>
2019-07-10 21:44:48 -0700

    Change LibStruct to use notations

--- All.v
+++ All.v
@@ -1,4 +1,5 @@
 Require Export AllNoNotations.
-Require Export KamiNotations.
+(* N.b.: this module exports notations or things dependent on them *)
+Require Export KamiNotations LibStruct.
 Export Word.Notations.
 Export ListNotations.
--- AllNoNotations.v
+++ AllNoNotations.v
@@ -1,2 +1,2 @@
 Require Export bbv.Word bbv.HexNotationWord Rtl Compile Lib.Fold Lib.EclecticLib Utila RecordUpdate.RecordSet.
-Require Export Syntax Properties PProperties PPlusProperties Extraction Tactics LibStruct.
+Require Export Syntax Properties PProperties PPlusProperties Extraction Tactics.
