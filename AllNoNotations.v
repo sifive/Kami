5908ff50dda35031991052cb2c804214d5b593fe
Jay Kruer <jaykru@protonmail.ch>
2019-07-10 19:32:43 -0700

    Move unfolded definitions to LibStruct

--- AllNoNotations.v
+++ AllNoNotations.v
@@ -1,2 +1,2 @@
 Require Export bbv.Word bbv.HexNotationWord Rtl Compile Lib.Fold Lib.EclecticLib Utila RecordUpdate.RecordSet.
-Require Export Syntax Properties PProperties PPlusProperties Extraction Tactics.
+Require Export Syntax Properties PProperties PPlusProperties Extraction Tactics LibStruct.
