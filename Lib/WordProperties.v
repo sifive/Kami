b85f95c3df6da87aa608da864ca03865abf53f20
swarnpriya <swarn.priya@sifive.com>
2019-07-19 15:08:07 -0700

    fixed more lemmas but need to add few more Ltac

--- Lib/WordProperties.v
+++ Lib/WordProperties.v
@@ -123,19 +123,6 @@ Ltac rewrite_bitwise := repeat (autorewrite with nat_bitwise_no_hyps).
 Ltac bitblast := repeat f_equal; eapply Z.bits_inj_iff; unfold Nat.eqf; intros; rewrite_bitwise.
 
 Lemma boundProofZ (sz : nat) (w : Z):
-  (w mod Z.of_nat (2^sz))%Z = w -> (w < Z.of_nat (2^sz))%Z.
-Proof.
-  intros sth0.
-  assert (forall sz', 0 < Z.of_nat (2 ^ sz'))%Z. {
-    induction sz'.
-    simpl. lia.
-    simpl. 
-    lia. }
-  specialize (Z.mod_pos_bound w _ (H sz)) as TMP; destruct TMP.
-  rewrite sth0 in *; assumption.
-Qed.
-
-Lemma boundProofZ' (sz : nat) (w : Z):
   (w mod (2^ Z.of_nat sz))%Z = w -> (w < (2^ Z.of_nat sz))%Z.
 Proof.
   intros sth0.
@@ -152,6 +139,31 @@ Proof.
   rewrite sth0 in *; assumption.
 Qed.
 
+Lemma gt0 (sz : nat) (w : Z) :
+  (w mod 2 ^ Z.of_nat sz >= 0)%Z.
+Proof.
+  destruct w.
+  destruct sz.
+  * simpl. rewrite Zmod_0_l. lia.
+  * simpl. rewrite Zmod_0_l. lia.
+  * simpl. destruct sz.
+    simpl. rewrite Zmod_1_r. lia.
+    rewrite Nat2Z.inj_succ.
+    rewrite <- Z.add_1_l.
+    rewrite Z.pow_add_r.
+    rewrite Z.pow_1_r.
+    admit.
+    lia.
+    lia.
+  * simpl. 
+    admit.
+Admitted.
+
+Ltac discharge_gt_0 :=
+  (destruct wordVal;
+  lia;
+  lia).
+
 Ltac arithmetizeWord :=
   repeat match goal with
          | w: word _ |- _ => destruct w
@@ -163,7 +175,7 @@ Ltac arithmetizeWord :=
          | |- ?w1 = ?w2 => match type of w1 with
                            | word ?sz => apply eq_wordVal
                            end; simpl
-         | H: ?w mod 2^(?sz) = ?w |- _ => pose proof (boundProofZ' sz _ H);
+         | H: ?w mod 2^(?sz) = ?w |- _ => pose proof (boundProofZ sz _ H);
                                           pose proof (inhabits H); clear H
          end; repeat match goal with
                      | H: inhabited ?P |- _ => destruct H
@@ -188,57 +200,40 @@ Proof.
   rewrite H1 in H.
   rewrite wordBound in H0.
   unfold Z.modulo; simpl.
-  assert (wordVal < 2)%Z.
-  simpl in *.
-  rewrite Z.pow_pos_fold in *.
-  pose (boundProofZ' _ _ wordBound).
-  admit.
-  lia.
-  pose (boundProof _ _ wordBound).
-  lia.
-  lia.
-  omega.
-  simpl.
-  simpl.
-  simpl Z.pow_pos.
-  simpl.
-  admit.
-  omega.
-  simpl in *.
-  rewrite Z.pow_pos_fold in *.
-  
+  assert (wordVal < Z.of_nat 2)%Z.
+  pose (boundProofZ _ _ wordBound).
+  apply l.
   lia.
 Qed.
 
-Lemma wor_wzero : forall sz w, wor _ (of_nat sz 0) w = w.
+
+Lemma wor_wzero : forall sz w, wor _ (ZToWord sz 0) w = w.
 Proof.
   intros.
   arithmetizeWord.
-  rewrite <- H0.
-  bitblast.
-  rewrite Nat.mod_0_l by discharge_pow_nonzero.
-  rewrite Nat.testbit_0_l; simpl.
-  rewrite H0.
-  auto.
+  assumption.
 Qed.
+
 
 Lemma unique_word_0 : forall a : word 0,
-    a = of_nat 0 0.
+    a = ZToWord 0 0.
 Proof.
   intros.
   arithmetizeWord.
   simpl in *.
-  auto.
+  rewrite Z.mod_1_r.
+  rewrite <- wordBound.
+  destruct wordVal.
+  * reflexivity.
+  * apply Z.mod_1_r.
+  * apply Z.mod_1_r.
 Qed.
 
-Lemma wzero_wplus: forall sz w, wadd _ (of_nat sz 0) w = w.
+Lemma wzero_wplus: forall sz w, wadd _ (ZToWord sz 0) w = w.
 Proof.
   intros.
   arithmetizeWord.
-  simpl in *.
-  rewrite Nat.mod_0_l by discharge_pow_nonzero.
-  simpl.
-  auto.
+  assumption.
 Qed.
 
 Lemma wor_idemp :  forall (n : nat) (x0 : word n), wor _ x0 x0 = x0.
@@ -247,14 +242,15 @@ Proof.
   induction x0.
   unfold wor.
   arithmetizeWord.
-  rewrite <- H0.
+  rewrite <- wordBound.
   repeat f_equal.
-  rewrite Nat.lor_diag.
+  rewrite Z.lor_diag.
   auto.
 Qed.
 
 Lemma foo : forall (n x : nat) (w1 w2 : word n),
-    wordVal _ (@truncMsb x _ w1) < wordVal _ (@truncMsb x _ w2) -> wordVal _ w1 < wordVal _ w2.
+    (wordVal _ (@truncMsb x _ w1) < wordVal _ (@truncMsb x _ w2))%Z ->
+    (wordVal _ w1 < wordVal _ w2)%Z.
 Proof.
   intros.
   arithmetizeWord.
