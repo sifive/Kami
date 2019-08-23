7fac99c537e376c8e2f06a227072dcb71a025be5
Murali Vijayaraghavan <vmurali@sifive.com>
2019-08-23 12:56:35 -0700

    removed Fin.t

--- All.v
+++ All.v
@@ -1,6 +1,3 @@
-Require Export Kami.AllDefn.
+Require Export Kami.AllNotations.
 (* N.b.: this module exports notations or things dependent on them *)
 Require Export Kami.Extraction.
-Require Export Kami.Notations Kami.LibStruct.
-Export Word.Notations.
-Export ListNotations.
