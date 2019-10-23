89c1b676fdff4a7c0bd7500ac0c258ab4b8947e3
Ken Roe <kendroe@hotmail.com>
2019-10-22 20:58:50 -0700

    Now that I see that the pull was accepted, I'm checking in Notations_rewrites on the rewrite_stuff branch.  Even though the current version does not compile, I would like to keep a version history and maintain a backup off of the laptop.

--- Notations_rewrites.v
+++ Notations_rewrites.v
@@ -9,8 +9,47 @@ Require Import Vector.
 Import VectorNotations.
 Import ListNotations.
 Require Import Kami.Notations.
+Require Import Coq.Logic.ClassicalFacts.
+Require Import Coq.Logic.Classical.
 
-Lemma makeModule_rules'_Registers: forall l, makeModule_rules' (Registers l)=[].
+Lemma app_rewrite1: forall T (a:T) b c, (a::b)++c=a::(b++c).
+Proof.
+  simpl.
+  intros.
+  reflexivity.
+Qed.
+
+Lemma Registers1: forall a b, Registers (a::b) = (MERegister a)::(Registers b).
+Proof.
+  intros.
+  simpl Registers.
+  reflexivity.
+Qed.
+
+Lemma Registers2: Registers []=[].
+Proof.
+  simpl.
+  reflexivity.
+Qed.
+
+Lemma Registers_dist_append : forall l1 l2, Registers (l1++l2)=(Registers l1)++(Registers l2).
+Proof.
+  intros.
+  induction l1.
+  + simpl.
+    reflexivity.
+  + simpl.
+    rewrite IHl1.
+    reflexivity.
+Qed.
+
+Lemma app_rewrite2: forall A (f:A) (r:list A), [f]++r=f::r.
+  Proof. reflexivity. Qed.
+
+Hint Rewrite app_rewrite1 app_rewrite2 app_nil_l app_nil_r Registers1 Registers2 : kami_rewrite_db.
+Hint Rewrite Registers1 Registers2 : kami_rewrite_db.
+
+Lemma makeModule_rules_Registers: forall l, makeModule_rules (Registers l)=[].
 Proof.
   intros.
   induction l.
@@ -22,36 +61,38 @@ Proof.
     reflexivity.
 Qed.
  
-Lemma makeModule_rules'_append: forall l1 l2, (makeModule_rules' (l1++l2))=(makeModule_rules' l1)++(makeModule_rules' l2).
-(*Proof.
+Lemma makeModule_rules_append: forall l1 l2, (makeModule_rules (l1++l2))=(makeModule_rules l1)++(makeModule_rules l2).
+Proof.
   intros.
   induction l1.
   + simpl.
     reflexivity.
   + simpl.
-    rewrite IHl1.*) Admitted.
+    rewrite IHl1.
+    destruct a; reflexivity.
+Qed.
 
-Lemma makeModule_rules'_MERegister: forall a b, makeModule_rules' ((MERegister a)::b)=makeModule_rules' b.
+Lemma makeModule_rules_MERegister: forall a b, makeModule_rules ((MERegister a)::b)=makeModule_rules b.
 Proof.
   simpl.
   reflexivity.
 Qed.
 
-Lemma makeModule_rules'_MERule: forall a b, makeModule_rules' ((MERule a)::b)=a::(makeModule_rules' b).
+Lemma makeModule_rules_MERule: forall a b, makeModule_rules ((MERule a)::b)=a::(makeModule_rules b).
 Proof.
   simpl.
   reflexivity.
 Qed.
 
-Lemma makeModule_rules'_nil: makeModule_rules' []=[].
+Lemma makeModule_rules_nil: makeModule_rules []=[].
 Proof.
   simpl.
   reflexivity.
 Qed.
 
-Hint Rewrite makeModule_rules'_Registers makeModule_rules'_append makeModule_rules'_MERegister makeModule_rules'_MERule makeModule_rules'_nil : makeModule_rules'_db.
+Hint Rewrite makeModule_rules_Registers makeModule_rules_append makeModule_rules_MERegister makeModule_rules_MERule makeModule_rules_nil : kami_rewrite_db.
  
-Lemma makeModule_meths'_Registers: forall l, makeModule_meths' (Registers l)=[].
+Lemma makeModule_meths_Registers: forall l, makeModule_meths (Registers l)=[].
 Proof.
   intros.
   induction l.
@@ -63,37 +104,39 @@ Proof.
   reflexivity.
 Qed.
  
-Lemma makeModule_meths'_append: forall l1 l2, makeModule_meths' (l1++l2)=(makeModule_meths' l1)++(makeModule_meths' l2).
-(*Proof.
+Lemma makeModule_meths_append: forall l1 l2, makeModule_meths (l1++l2)=(makeModule_meths l1)++(makeModule_meths l2).
+Proof.
   intros.
   induction l1.
   + simpl.
     reflexivity.
   + simpl.
-    rewrite IHl1.*) Admitted.
+    rewrite IHl1.
+    destruct a; reflexivity.
+Qed.
 
-Lemma makeModule_meths'_MERegister: forall a b, makeModule_meths' ((MERegister a)::b)=makeModule_meths' b.
+Lemma makeModule_meths_MERegister: forall a b, makeModule_meths ((MERegister a)::b)=makeModule_meths b.
 Proof.
   simpl.
   reflexivity.
 Qed.
 
-Lemma makeModule_meths'_MERule: forall a b, makeModule_meths' ((MERule a)::b)=(makeModule_meths' b).
+Lemma makeModule_meths_MERule: forall a b, makeModule_meths ((MERule a)::b)=(makeModule_meths b).
 Proof.
   simpl.
   reflexivity.
 Qed.
 
-Lemma makeModule_meths'_nil: makeModule_meths' []=[].
+Lemma makeModule_meths_nil: makeModule_meths []=[].
 Proof.
   simpl.
   reflexivity.
 Qed.
 
-Hint Rewrite makeModule_meths'_Registers makeModule_meths'_append makeModule_meths'_MERegister makeModule_meths'_MERule makeModule_meths'_nil : makeModule_meths'_db.
+Hint Rewrite makeModule_meths_Registers makeModule_meths_append makeModule_meths_MERegister makeModule_meths_MERule makeModule_meths_nil : kami_rewrite_db.
 
  
-Lemma makeModule_regs'_Registers: forall l, makeModule_regs' (Registers l)=l.
+Lemma makeModule_regs_Registers: forall l, makeModule_regs (Registers l)=l.
 Proof.
   intros.
   induction l.
@@ -105,32 +148,327 @@ Proof.
     reflexivity.
 Qed.
  
-Lemma makeModule_regs'_append: forall l1 l2, makeModule_regs' (l1++l2)=(makeModule_regs' l1)++(makeModule_regs' l2).
-(*Proof.
+Lemma makeModule_regs_append: forall l1 l2, makeModule_regs (l1++l2)=(makeModule_regs l1)++(makeModule_regs l2).
+Proof.
   intros.
   induction l1.
   + simpl.
     reflexivity.
   + simpl.
-    rewrite IHl1.*) Admitted.
+    rewrite IHl1.
+    destruct a; reflexivity.
+Qed.
 
-Lemma makeModule_regs'_MERegister: forall a b, makeModule_regs' ((MERegister a)::b)=a::(makeModule_regs' b).
+
+Lemma makeModule_regs_MERegister: forall a b, makeModule_regs ((MERegister a)::b)=a::(makeModule_regs b).
 Proof.
   simpl.
   reflexivity.
 Qed.
 
-Lemma makeModule_regs'_MERule: forall a b, makeModule_regs' ((MERule a)::b)=makeModule_regs' b.
+Lemma makeModule_regs_MERule: forall a b, makeModule_regs ((MERule a)::b)=makeModule_regs b.
 Proof.
   simpl.
   reflexivity.
 Qed.
 
-Lemma makeModule_regs'_nil: makeModule_regs' []=[].
+Lemma makeModule_regs_nil: makeModule_regs []=[].
 Proof.
   simpl.
   reflexivity.
 Qed.
 
-Hint Rewrite makeModule_regs'_Registers makeModule_regs'_append makeModule_regs'_MERegister makeModule_regs'_MERule makeModule_regs'_nil : makeModule_regs'_db.
+Hint Rewrite makeModule_regs_Registers makeModule_regs_append makeModule_regs_MERegister makeModule_regs_MERule makeModule_regs_nil : kami_rewrite_db.
+
+Lemma map1: forall T R (f: T -> R) (h:T) t, List.map f (h::t)=(f h)::(List.map f t).
+Proof.
+  simpl.
+  reflexivity.
+Qed.
+
+Lemma getAllRules_ConcatMod : forall a b, getAllRules (ConcatMod a b)=getAllRules a++getAllRules b.
+Proof.
+  intros.
+  simpl.
+  reflexivity.
+Qed.
+
+Lemma getAllMethods_ConcatMod : forall a b, getAllMethods (ConcatMod a b)=getAllMethods a++getAllMethods b.
+Proof.
+  intros.
+  simpl.
+  reflexivity.
+Qed.
+
+(*
+Lemma getCallsWithSignPerRule_append: forall T a b, getCallsWithSignPerRule (a++b)=getCallsWithSignPerRule a++getCallsWithSignPerRule b.
+Proof.*)
+
+Lemma getCallsPerMod_ConcatMod : forall a b, getCallsPerMod (ConcatMod a b)=(getCallsPerMod a)++(getCallsPerMod b).
+Proof.
+  unfold getCallsPerMod.
+  simpl.
+  intros.
+  rewrite map_app.
+  reflexivity.
+Qed.
+
+Lemma getCallsPerMod_BaseRegFile: forall m,
+  getCallsPerMod (Base (BaseRegFile m)) = [].
+Proof.
+  intros.
+  unfold getCallsPerMod.
+  unfold getCallsWithSignPerMod.
+  simpl.
+  unfold getRegFileMethods.
+  destruct m.
+  destruct rfRead.
+  + simpl.
+    unfold getCallsWithSignPerMeth.
+    destruct rfIsWrMask.
+    - simpl.
+      unfold readRegFile.
+      induction reads.
+      * reflexivity.
+      * simpl.
+        rewrite IHreads.
+        reflexivity.
+    - simpl.
+      unfold readRegFile.
+      induction reads.
+      * reflexivity.
+      * simpl.
+        rewrite IHreads.
+        reflexivity.
+  + simpl.
+    unfold getCallsWithSignPerMeth.
+    simpl.
+    unfold readSyncRegFile.
+    destruct rfIsWrMask.
+    simpl.
+    destruct isAddr.
+    * simpl.
+      induction reads.
+      -- reflexivity.
+      -- simpl.
+         rewrite map_app in IHreads.
+         rewrite map_app.
+         rewrite map_cons.
+         simpl.
+         rewrite concat_app in IHreads.
+         rewrite concat_app.
+         rewrite concat_cons.
+         rewrite app_nil_l.
+         rewrite IHreads.
+         reflexivity.
+    * simpl.
+      induction reads.
+      -- reflexivity.
+      -- simpl.
+         rewrite map_app in IHreads.
+         rewrite map_app.
+         rewrite map_cons.
+         simpl.
+         rewrite concat_app in IHreads.
+         rewrite concat_app.
+         rewrite concat_cons.
+         rewrite app_nil_l.
+         rewrite IHreads.
+         reflexivity.
+    * simpl.
+      destruct isAddr.
+      -- simpl.
+         induction reads.
+         ++ reflexivity.
+         ++ simpl.
+            rewrite map_app in IHreads.
+            rewrite map_app.
+            rewrite map_cons.
+            simpl.
+            rewrite concat_app in IHreads.
+            rewrite concat_app.
+            rewrite concat_cons.
+            rewrite app_nil_l.
+            rewrite IHreads.
+            reflexivity.
+      -- simpl.
+         induction reads.
+         ++ reflexivity.
+         ++ simpl.
+            rewrite map_app in IHreads.
+            rewrite map_app.
+            rewrite map_cons.
+            simpl.
+            rewrite concat_app in IHreads.
+            rewrite concat_app.
+            rewrite concat_cons.
+            rewrite app_nil_l.
+            rewrite IHreads.
+            reflexivity.
+Qed.
+
+  Lemma getCallsPerMod_Base: forall (m : BaseModule), getCallsPerMod (Base m)=List.map fst (getCallsWithSignPerMod m).
+  Proof.
+    unfold getCallsPerMod.
+    reflexivity.
+  Qed.
+
+Lemma map_getCallsPerMod_map_BaseRegFile: forall l,
+  (concat (List.map getCallsPerMod
+     (List.map (fun m : RegFileBase => (Base (BaseRegFile m))) l)))=[].
+Proof.
+  intros.
+  induction l.
+  + reflexivity.
+  + simpl.
+    rewrite IHl.
+    rewrite app_nil_r.
+    rewrite <- getCallsPerMod_Base.
+    rewrite getCallsPerMod_BaseRegFile.
+    reflexivity.
+Qed.
+
+  Lemma getCallsPerMod_fold_right_ConcatMod: forall (a:Mod) (l:list Mod), getCallsPerMod (List.fold_right ConcatMod a l)=concat (List.map getCallsPerMod l)++(getCallsPerMod a).
+  Proof.
+    intros.
+    induction l.
+    + reflexivity.
+    + simpl.
+      rewrite IHl.
+      rewrite app_assoc.
+      reflexivity. 
+Qed.
+     
+  (*Lemma getCallsPerMod_BaseRegFile: forall m, getCallsPerMod (BaseRegFile m)=[].
+  Admitted.*)
+
+  Definition isSubModule (x: Mod) (y: Mod) := False.
+
+  Theorem isSubModule_ConcatMod1: forall m x y, isSubModule m x -> isSubModule m (ConcatMod x y).
+  Admitted.
+
+  Theorem isSubModule_ConcatMod2: forall m x y,  isSubModule m y -> isSubModule m (ConcatMod x y).
+  Admitted.
+
+  Theorem isSubModule_fold_right_ConcatMod: forall m x yl, isSubModule m x -> isSubModule m (List.fold_right ConcatMod x yl).
+  Admitted.
+
+  Theorem isSubModule_self: forall m n, m=n -> isSubModule m n.
+  Admitted.
+
+  Theorem wfMod_createHideMod : forall x m, isSubModule m x -> WfMod x -> WfMod (createHideMod x (getCallsPerMod m)).
+  Admitted.
+
+  Hint Rewrite map1 getAllRules_ConcatMod getAllMethods_ConcatMod getCallsPerMod_ConcatMod map_getCallsPerMod_map_BaseRegFile : kami_rewrite_db.
+  Hint Rewrite getCallsPerMod_fold_right_ConcatMod getCallsPerMod_BaseRegFile isSubModule_ConcatMod1 isSubModule_ConcatMod2 : kami_rewrite_db.
+  Hint Rewrite isSubModule_fold_right_ConcatMod isSubModule_self : kami_rewrite_db.
+
+  Theorem getAllRegisters_ConcatMod: forall a b, getAllRegisters (ConcatMod a b)=getAllRegisters(a)++getAllRegisters(b).
+  Proof.
+     reflexivity.
+  Qed.
+
+  Theorem missing_prop: forall x y, (x=y)=(x<->y).
+  Admitted.
+
+  Theorem DisjKey_Cons1:
+    forall T Q (a:(T*Q)) x z, DisjKey (a::x) z = ((~(List.In (fst a) (List.map fst z))) /\ DisjKey x z).
+  Proof.
+    intros.
+    unfold DisjKey.
+    simpl.
+    rewrite missing_prop.
+    split.
+    + intros.
+      split.
+      - assert (~(fst a=fst a \/ List.In (fst a) (List.map fst x)) \/
+                ~ List.In (fst a) (List.map fst z)).
+        apply H.
+        inversion H0; subst; clear H0.
+        tauto.
+        tauto.
+      - simpl.
+        intros.
+        assert (~(fst a=k \/ List.In k (List.map fst x)) \/
+                ~ List.In k (List.map fst z)).
+        apply H.
+        inversion H0; subst; clear H0.
+        apply not_or_and in H1.
+        inversion H1; subst; clear H1.
+        left.
+        apply H2.
+        right.
+        apply H1.
+    + intros.
+      inversion H; subst; clear H.
+      classical_right.
+      apply NNPP in H.
+      inversion H; subst; clear H.
+      - apply H0.
+      - simpl.
+      assert (~ List.In k (List.map fst x) \/ ~ List.In k (List.map fst z)).
+      apply H1.
+      inversion H; subst; clear H.
+      apply H3 in H2.
+      inversion H2.
+      apply H3.
+Qed.
+
+Theorem DisjKey_Append1: forall T Q (x:list (T*Q)) (y:list (T*Q)) (z:list (T*Q)), DisjKey (x++y) z=(DisjKey x z /\ DisjKey y z).
+  Proof.
+    intros.
+    induction x.
+    + simpl.
+      unfold DisjKey.
+      simpl.
+      rewrite missing_prop.
+      split.
+      - intros.
+        * split.
+          tauto.
+          apply H.
+      - intros.
+        inversion H. subst. clear H.
+        apply H1.
+    + simpl.
+      rewrite ?DisjKey_Cons1.
+      rewrite missing_prop.
+      split.
+      intros.
+      inversion H; subst; clear H.
+      split.
+      - split.
+        apply H0.
+        rewrite IHx in H1.
+        inversion H1; subst; clear H1.
+        apply H.
+      - simpl.
+        rewrite IHx in H1.
+        inversion H1; subst; clear H1.
+        apply H2.
+      - simpl.
+        intros.
+        inversion H; subst; clear H.
+        split.
+        * 
+  Admitted.
+
+  Theorem DisjKey_Append2: forall T Q (x:list (T*Q)) (y:list (T*Q)) (z:list (T*Q)), DisjKey x (y++z)=(DisjKey x y /\ DisjKey x z).
+  Admitted.
+
+  Theorem DisjKey_In_map2: forall A B a (k:A) r l, @DisjKey A B a ((k,r)::l)=(~List.In k (List.map fst a) /\ (DisjKey a l)).
+  Admitted.
+    
+  Theorem DisjKey_In_map1: forall A B b (k:A) r l, @DisjKey A B ((k,r)::l) b=(~List.In k (List.map fst b) /\ (DisjKey l b)).
+  Admitted.
+
+  Theorem DisjKey_In_map_fst2: forall A B a (f:(A*B)) l, @DisjKey A B a (f::l)=(~List.In (fst f) (List.map fst a) /\ (DisjKey a l)).
+  Admitted.
+    
+  Theorem DisjKey_In_map_fst1: forall A B b (f:(A*B)) l, @DisjKey A B (f::l) b=(~List.In (fst f) (List.map fst b) /\ (DisjKey l b)).
+  Admitted.
+
+  Hint Rewrite getAllRegisters_ConcatMod DisjKey_Append1 DisjKey_Append2 DisjKey_In_map2 DisjKey_In_map1 : kami_rewrite_db.
+  Hint Rewrite DisjKey_In_map_fst2 DisjKey_In_map_fst1: kami_rewrite_db.
+
 
