0a07cc90425dc31e9ef875fd1507dc80cf4cff2d
Murali Vijayaraghavan <vmurali@sifive.com>
2019-11-01 17:49:03 -0700

    removed old compiler

--- Extraction.v
+++ Extraction.v
@@ -1,5 +1,5 @@
 Require Export List String Ascii.
-Require Export Kami.Syntax Kami.Compiler.Compile Kami.Compiler.CompilerSimple Kami.Compiler.Compiler Kami.Compiler.Rtl Kami.LibStruct.
+Require Export Kami.Syntax Kami.Compiler.CompilerSimple Kami.Compiler.Compiler Kami.Compiler.Rtl Kami.LibStruct.
 
 Require Import Kami.Notations.
 
