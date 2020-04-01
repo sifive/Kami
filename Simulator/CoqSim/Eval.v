df7a4376ed4b0e1e05f02f11ccfa60c0051c26a4
Evan Marzion <evan.marzion@sifive.com>
2020-04-01 13:10:13 -0700

    removing 0 special case as well

--- Simulator/CoqSim/Eval.v
+++ Simulator/CoqSim/Eval.v
@@ -29,10 +29,7 @@ Definition print_BF(bf : BitFormat){n} : BV n -> string :=
 Fixpoint print_Val(k : Kind)(ff : FullFormat k) : eval_Kind k -> string :=
   match ff with
   | FBool n _ => fun x => pad_with " " n (if x then "1" else "0")
-  | FBit n m bf => fun x => match m with
-                            | 0 => print_BF bf x
-                            | _ => pad_with "0" m (print_BF bf x)
-                            end
+  | FBit n m bf => fun x => pad_with "0" m (print_BF bf x)
   | FStruct n fk fs ffs => fun x => ("{ " ++ String.concat "; " (v_to_list (vmap (fun '(str1,str2) => str1 ++ ":" ++ str2) (add_strings fs (tup_to_vec _ (fun i => print_Val (ffs i)) x)))) ++ "; }")%string
   | FArray n k' ff' => fun x => ("[" ++ String.concat "; " (List.map (fun i => nat_decimal_string (f2n i) ++ "=" ++ print_Val ff' (vector_index i x)) (getFins n)) ++ "; ]")%string
   end.
