0c12961640dd938fb75b18a78a56a17a16189de1
Ken Roe <kendroe@hotmail.com>
2020-04-07 16:33:17 -0400

    Separated out SimplifyProp from other reflections

--- Rewrites/ReflectionSoundTheorems2.v
+++ Rewrites/ReflectionSoundTheorems2.v
@@ -10,15 +10,13 @@ Require Import Kami.Rewrites.ReflectionSoundTheorems1.
 (************************************************************************************************************)
 
 Scheme KRExpr_list_string_mut := Induction for KRExpr_list_string Sort Prop
-  with KRExpr_list_list_string_mut := Induction for KRExpr_list_list_string Sort Prop
-  with KRExpr_Prop_mut := Induction for KRExpr_Prop Sort Prop.
+  with KRExpr_list_list_string_mut := Induction for KRExpr_list_list_string Sort Prop.
 
 Ltac KRSimplifySound_setup3 mut H H0 H1 :=
   intros;
   eapply (mut
             (fun e : KRExpr_list_string => KRExprDenote_list_string (KRSimplify_list_string e) = KRExprDenote_list_string e)
             (fun e : KRExpr_list_list_string => KRExprDenote_list_list_string (KRSimplify_list_list_string e) = KRExprDenote_list_list_string e)
-            (fun e : KRExpr_Prop => KRExprDenote_Prop (KRSimplify_Prop e) = KRExprDenote_Prop e)
          );
   try (try (intros);try(autorewrite with KRSimplify); try(autorewrite with KRSimplifyTopSound); try(simpl); try (rewrite H); try  (rewrite H0); try (rewrite H1); reflexivity).
 
@@ -88,125 +86,12 @@ Qed.
 
 Hint Rewrite KRSimplify_list_list_string_KRApp_list_list_string : KRSimplify.
 
-Theorem KRSimplify_Prop_KRAnd_Prop: forall a b,
-     KRSimplify_Prop (KRAnd_Prop a b)= KRSimplifyTop_Prop (KRAnd_Prop (KRSimplify_Prop a) (KRSimplify_Prop b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KRAnd_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KROr_Prop: forall a b,
-     KRSimplify_Prop (KROr_Prop a b)= KRSimplifyTop_Prop (KROr_Prop (KRSimplify_Prop a) (KRSimplify_Prop b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KROr_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KRNot_Prop: forall a,
-     KRSimplify_Prop (KRNot_Prop a)= KRSimplifyTop_Prop (KRNot_Prop (KRSimplify_Prop a)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KRNot_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KRIn_string_Prop: forall a b,
-     KRSimplify_Prop (KRIn_string_Prop a b)= KRSimplifyTop_Prop (KRIn_string_Prop (KRSimplify_string a) (KRSimplify_list_string b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KRIn_string_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KRIn_RegInitT_Prop: forall a b,
-     KRSimplify_Prop (KRIn_RegInitT_Prop a b)= KRSimplifyTop_Prop (KRIn_RegInitT_Prop (KRSimplify_RegInitT a) (KRSimplify_list_RegInitT b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KRIn_RegInitT_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KRIn_DefMethT_Prop: forall a b,
-     KRSimplify_Prop (KRIn_DefMethT_Prop a b)= KRSimplifyTop_Prop (KRIn_DefMethT_Prop (KRSimplify_DefMethT a) (KRSimplify_list_DefMethT b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KRIn_DefMethT_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KRIn_Rule_Prop: forall a b,
-     KRSimplify_Prop (KRIn_Rule_Prop a b)= KRSimplifyTop_Prop (KRIn_Rule_Prop (KRSimplify_Rule a) (KRSimplify_list_Rule b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KRIn_Rule_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KRDisjKey_RegInitT_Prop: forall a b,
-     KRSimplify_Prop (KRDisjKey_RegInitT a b)= KRSimplifyTop_Prop (KRDisjKey_RegInitT (KRSimplify_list_RegInitT a) (KRSimplify_list_RegInitT b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KRDisjKey_RegInitT_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KRDisjKey_DefMethT_Prop: forall a b,
-     KRSimplify_Prop (KRDisjKey_DefMethT a b)= KRSimplifyTop_Prop (KRDisjKey_DefMethT (KRSimplify_list_DefMethT a) (KRSimplify_list_DefMethT b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KRDisjKey_DefMethT_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KRDisjKey_Rule_Prop: forall a b,
-     KRSimplify_Prop (KRDisjKey_Rule a b)= KRSimplifyTop_Prop (KRDisjKey_Rule (KRSimplify_list_Rule a) (KRSimplify_list_Rule b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KRDisjKey_Rule_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KREq_string_Prop: forall a b,
-     KRSimplify_Prop (KREq_string_Prop a b)= KRSimplifyTop_Prop (KREq_string_Prop (KRSimplify_string a) (KRSimplify_string b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KREq_string_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KREq_RegInitT_Prop: forall a b,
-     KRSimplify_Prop (KREq_RegInitT_Prop a b)= KRSimplifyTop_Prop (KREq_RegInitT_Prop (KRSimplify_RegInitT a) (KRSimplify_RegInitT b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KREq_RegInitT_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KREq_DefMethT_Prop: forall a b,
-     KRSimplify_Prop (KREq_DefMethT_Prop a b)= KRSimplifyTop_Prop (KREq_DefMethT_Prop (KRSimplify_DefMethT a) (KRSimplify_DefMethT b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KREq_DefMethT_Prop : KRSimplify.
-
-Theorem KRSimplify_Prop_KREq_Rule_Prop: forall a b,
-     KRSimplify_Prop (KREq_Rule_Prop a b)= KRSimplifyTop_Prop (KREq_Rule_Prop (KRSimplify_Rule a) (KRSimplify_Rule b)).
-Proof.
-  reflexivity.
-Qed.
-
-Hint Rewrite KRSimplify_Prop_KREq_Rule_Prop : KRSimplify.
-
 Theorem KRSimplifySound_list_string: forall e,
     KRExprDenote_list_string (KRSimplify_list_string e) = KRExprDenote_list_string e.
 Proof.
    eapply (KRExpr_list_string_mut
             (fun e : KRExpr_list_string => KRExprDenote_list_string (KRSimplify_list_string e) = KRExprDenote_list_string e)
             (fun e : KRExpr_list_list_string => KRExprDenote_list_list_string (KRSimplify_list_list_string e) = KRExprDenote_list_list_string e)
-            (fun e : KRExpr_Prop => KRExprDenote_Prop (KRSimplify_Prop e) = KRExprDenote_Prop e)
           ); try (intros; autorewrite with KRSimplify;simpl;try(f_equal);autorewrite with KRSimplify;try (apply H);try (apply H0);reflexivity).
 Qed.
 
@@ -218,21 +103,8 @@ Proof.
    eapply (KRExpr_list_list_string_mut
             (fun e : KRExpr_list_string => KRExprDenote_list_string (KRSimplify_list_string e) = KRExprDenote_list_string e)
             (fun e : KRExpr_list_list_string => KRExprDenote_list_list_string (KRSimplify_list_list_string e) = KRExprDenote_list_list_string e)
-            (fun e : KRExpr_Prop => KRExprDenote_Prop (KRSimplify_Prop e) = KRExprDenote_Prop e)
           ); try (intros; autorewrite with KRSimplify;simpl;try(f_equal);autorewrite with KRSimplify;try (apply H);try (apply H0);reflexivity).
 Qed.
 
 Hint Rewrite KRSimplifySound_list_list_string : KRSimplify.
-
-Theorem KRSimplifySound_Prop: forall e,
-    KRExprDenote_Prop (KRSimplify_Prop e) = KRExprDenote_Prop e.
-Proof.
-   eapply (KRExpr_Prop_mut
-            (fun e : KRExpr_list_string => KRExprDenote_list_string (KRSimplify_list_string e) = KRExprDenote_list_string e)
-            (fun e : KRExpr_list_list_string => KRExprDenote_list_list_string (KRSimplify_list_list_string e) = KRExprDenote_list_list_string e)
-            (fun e : KRExpr_Prop => KRExprDenote_Prop (KRSimplify_Prop e) = KRExprDenote_Prop e)
-          ); try (intros; autorewrite with KRSimplify;simpl;try(f_equal);autorewrite with KRSimplify;try (apply H);try (apply H0);reflexivity).
-Qed.
-
-Hint Rewrite KRSimplifySound_Prop : KRSimplify.
 
